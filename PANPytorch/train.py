9162ab1436256b0a053f5d228c0b7c2011e7d82e
rachikbilal <brachik@bigapps.fr>
2020-09-09 10:22:12 +0200

    'init'

---
+++ PANPytorch/train.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/23 22:00
+# @Author  : zhoujun
+
+from __future__ import print_function
+import os
+from PANPytorch.utils import load_json
+
+config = load_json('config.json')
+
+from PANPytorch.models import get_model, get_loss
+from PANPytorch.data_loader import get_dataloader
+from PANPytorch.trainer import Trainer
+
+
+def main(config):
+    train_loader = get_dataloader(config['data_loader']['type'], config['data_loader']['args'])
+
+    criterion = get_loss(config).cuda()
+
+    model = get_model(config)
+
+    trainer = Trainer(config=config,
+                      model=model,
+                      criterion=criterion,
+                      train_loader=train_loader)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main(config)
